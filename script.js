bb42069b0f665f8224dee14d2bd83b0fa294e4f9
2lag <aidenhelfman@gmail.com>
2023-08-06 03:16:26 -0500

    javascript cleanup & prep for background

--- script.js
+++ script.js
@@ -4,62 +4,28 @@ function toggle_options( ) {
   options.classList.toggle( "options-open" );
 }
 
-var paragraph_container = document.getElementById("hidden_paragraph");
-var software = document.getElementsByClassName("hidden_txt")[0];
-var software_sub = document.getElementsByClassName("hidden_txt")[1];
-var social = document.getElementsByClassName("hidden_txt")[2];
-var social_sub = document.getElementsByClassName("hidden_txt")[3];
-var marketing = document.getElementsByClassName("hidden_txt")[4];
-var marketing_sub = document.getElementsByClassName("hidden_txt")[5];
-var cyber = document.getElementsByClassName("hidden_txt")[6];
-var cyber_sub = document.getElementsByClassName("hidden_txt")[7];
+var paragraph_container = document.getElementById( "hidden_paragraph" );
+const paragraphs = document.getElementsByClassName( "hidden_txt" );
 var activeButton = null;
 
-// Hide all the elements initially
-hideAllElements();
-
 function hideAllElements() {
-  software.style.display = "none";
-  software_sub.style.display = "none";
-  social.style.display = "none";
-  social_sub.style.display = "none";
-  marketing.style.display = "none";
-  marketing_sub.style.display = "none";
-  cyber.style.display = "none";
-  cyber_sub.style.display = "none";
+  for( var i = 0; i < paragraphs.length; i++ ) {
+    paragraphs[ i ].style.display = "none";
+  }
 }
 
-function show_paragraph(num) {
-  var currentButton = null;
-  var currentParagraph = null;
+hideAllElements();
 
-  switch (num) {
-    case 1:
-      currentButton = software;
-      currentParagraph = software_sub;
-      break;
-    case 2:
-      currentButton = social;
-      currentParagraph = social_sub;
-      break;
-    case 3:
-      currentButton = marketing;
-      currentParagraph = marketing_sub;
-      break;
-    case 4:
-      currentButton = cyber;
-      currentParagraph = cyber_sub;
-      break;
-  }
+function show_paragraph( num ) {
+  var currentParagraph = paragraphs[ num ].nextElementSibling;
+  var currentButton = paragraphs[ num ];
 
-  if (activeButton === currentButton) {
-    // Hide the container and reset styles if the same button is pressed twice
+  if( activeButton === currentButton ) {
     paragraph_container.style.top = "1000px";
     paragraph_container.style.opacity = "0";
     activeButton = null;
   } else {
-    // Show the selected button's paragraph and subtext
-    hideAllElements(); // Hide all elements before showing the selected ones
+    hideAllElements();
     currentButton.style.display = "block";
     currentParagraph.style.display = "block";
     paragraph_container.style.top = "1250px";
