b5e03ddf38516c2d099bdf6d9a60e64d4b593727
2lag <aidenhelfman@gmail.com>
2023-08-06 23:53:26 -0500

    added glowing scrolling anim

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+// options pop out
 var options = document.getElementsByClassName( "options" )[ 0 ];
-
 function toggle_options( ) {
   options.classList.toggle( "options-open" );
 }
 
-var paragraph_container = document.getElementById( "hidden_paragraph" );
+// services drop down
 const paragraphs = document.getElementsByClassName( "hidden_txt" );
-var activeButton = null;
-
 function hideAllElements() {
   for( var i = 0; i < paragraphs.length; i++ ) {
     paragraphs[ i ].style.display = "none";
   }
 }
-
 hideAllElements();
 
+var paragraph_container = document.getElementById( "hidden_paragraph" );
+var activeButton = null;
 function show_paragraph( num ) {
   var currentParagraph = paragraphs[ num ].nextElementSibling;
   var currentButton = paragraphs[ num ];
@@ -34,3 +33,24 @@ function show_paragraph( num ) {
     activeButton = currentButton;
   }
 }
+
+// background highlight
+var scroll_percent_mapped = 0;
+var background = document.getElementById( "background" );
+function move_highlight( ) {
+  const top = 0;
+  const bottom = 3100 - window.innerHeight;
+  const scroll_percent = ( window.scrollY - top ) / ( bottom - top ) * 100;
+  scroll_percent_mapped = scroll_percent * ( window.innerHeight / 100 ) + window.scrollY;
+  background.style.backgroundImage = 'linear-gradient( to bottom, rgba( 255, 255, 255, 0 ) ' + ( scroll_percent_mapped - 50 ) + 'px, rgba( 255, 255, 255, .777 ) ' + scroll_percent_mapped + 'px, rgba( 255, 255, 255, 0 ) ' + ( scroll_percent_mapped + 50 ) + 'px ), url("./img/background.png")';
+}
+window.addEventListener( "scroll", move_highlight );
+
+const delay = ms => new Promise( res => setTimeout( res, ms ) );
+const loopprint = async ( ) => {
+  while( true ) {
+    await delay( 1000 );
+    console.log( scroll_percent_mapped );
+  }
+}
+loopprint( );
