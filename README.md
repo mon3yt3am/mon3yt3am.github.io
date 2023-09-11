a604165a1a9bb330dc769d9fa02c8b064310ccea
2lag <aidenhelfman@gmail.com>
2023-09-10 21:40:45 -0500

    more quick fixes test across browsers to verify work

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # [mon3yt3am](https://mon3yt3am.github.io)
 
 ## todo:
-- [ ] convert all images to webp
-- [ ] add survey
-- [ ] add FAQ
+- [ ] fix bounds/stuff being covered on different browsers/sizes
 - [ ] update new windows with S.N.A.P. description
 - [ ] add click window popup for each about us popup
+- [ ] add survey
+- [ ] add FAQ
 - [ ] create link for the booking service for free 1/2 hour consultation
 - [ ] add light mode switch
--- js/short.js
+++ js/short.js
@@ -6,13 +6,16 @@ var desccont = document.getElementsByClassName( "description_container" )[0];
 document.documentElement.style.height = window.innerHeight + "px";
 bksize.style.height = window.innerHeight + "px";
 bkcont.style.height = window.innerHeight + "px";
-desccont.style.top = ( window.innerHeight / 2 ) + "px";
+if( this.window.innerWidth <= 705 )
+  desccont.style.top = ( ( window.innerHeight / 2 ) + 115 ) + "px";
+else
+  desccont.style.top = ( window.innerHeight / 2 ) + "px";
 
 window.addEventListener( "resize", function() {
   document.documentElement.style.height = window.innerHeight + "px";
   bksize.style.height = window.innerHeight + "px";
   bkcont.style.height = window.innerHeight + "px";
-  if( this.window.innerWidth <= 490 )
+  if( this.window.innerWidth <= 705 )
     desccont.style.top = ( ( window.innerHeight / 2 ) + 115 ) + "px";
   else
     desccont.style.top = ( window.innerHeight / 2 ) + "px";
