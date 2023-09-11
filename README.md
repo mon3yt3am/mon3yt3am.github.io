2c928a87465fdbd0432db9974f82437323c15e44
2lag <aidenhelfman@gmail.com>
2023-09-10 21:27:19 -0500

    hotfixes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # [mon3yt3am](https://mon3yt3am.github.io)
 
 ## todo:
+- [ ] convert all images to webp
+- [ ] add survey
+- [ ] add FAQ
 - [ ] update new windows with S.N.A.P. description
 - [ ] add click window popup for each about us popup
 - [ ] create link for the booking service for free 1/2 hour consultation
--- js/short.js
+++ js/short.js
@@ -1,5 +1,19 @@
-// set <html> tag style height to window size on load and resize
+// set <html> and background height to window size on load and resize
+var bksize = document.getElementById( "background" );
+var bkcont = document.getElementById( "background_container" );
+var desccont = document.getElementsByClassName( "description_container" )[0];
+
 document.documentElement.style.height = window.innerHeight + "px";
+bksize.style.height = window.innerHeight + "px";
+bkcont.style.height = window.innerHeight + "px";
+desccont.style.top = ( window.innerHeight / 2 ) + "px";
+
 window.addEventListener( "resize", function() {
   document.documentElement.style.height = window.innerHeight + "px";
+  bksize.style.height = window.innerHeight + "px";
+  bkcont.style.height = window.innerHeight + "px";
+  if( this.window.innerWidth <= 490 )
+    desccont.style.top = ( ( window.innerHeight / 2 ) + 115 ) + "px";
+  else
+    desccont.style.top = ( window.innerHeight / 2 ) + "px";
 } );
